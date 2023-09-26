5b2a9c3bb3eb47c9c996259e52a83ea79ac2ccba
Youssef Girgeis <youssef_girgeis@hotmail.com>
2023-09-25 20:24:40 -0400

    add homepage route - default

--- app.js
+++ app.js
@@ -37,6 +37,9 @@ app.use(xss());
 // extra packages
 
 // routes
+app.get('/', (req, res) => {
+  res.send('Jobs API');
+});
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
